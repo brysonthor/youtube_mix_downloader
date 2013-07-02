6bab14c1bd307a1bc308dba469a2b9e29d9bc8d3
Bryson Thornock <bthor@chem.byu.edu>
2013-07-02 11:27:23 -0600

    Initial Working Release

--- down_mix.py
+++ down_mix.py
@@ -11,13 +11,13 @@ import string
 def clean_name(title):
     #some stackoverflow fun, adapted of course
     valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-    cleanedFilename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore')
+    cleanedFilename = unicodedata.normalize('NFKD', title).encode('ASCII', 'ignore')
     return ''.join(c for c in cleanedFilename if c in valid_chars)
 
 def main():
     BASE_URL= 'http://www.youtube.com/watch?v='
     EXT = '.mp3'
-    TEMP_VID = '~/.youtube-dl-%s.flv'
+    TEMP_VID = '.youtube-dl-%s.flv'
     LOWER = 100000
     UPPER = 999999
     r = requests.get(argv[1])
@@ -33,9 +33,9 @@ def main():
 
     for v in videoUrlList:
         temp_file = TEMP_VID % random.randint(LOWER,UPPER)
-        youtube_cmd = 'youtube-dl --output=%s --format=18 "%s"' %(temp_file,v[0])
-        conv_cmd = 'ffmpeg -i %s -acodec libmp3lame -ac 2 -ab 320k -vn -y "%s"' %(temp_file,clean_name(v[1]))
-
+        youtube_cmd = 'youtube-dl --output="%s" --format=18 "%s"' %(temp_file,v[0])
+        conv_cmd = 'ffmpeg -i "%s" -acodec libmp3lame -ac 2 -ab 320k -vn -y "%s"' %(temp_file,clean_name(v[1]))
+        #print temp_file,youtube_cmd,conv_cmd
         os.system(youtube_cmd)
         os.system(conv_cmd)
         os.remove(temp_file)
