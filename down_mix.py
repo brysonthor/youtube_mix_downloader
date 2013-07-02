50e721c5d4b4005aef33d66863b548097e479888
Bryson Thornock <bthor@chem.byu.edu>
2013-07-02 11:06:34 -0600

    more robust

--- down_mix.py
+++ down_mix.py
@@ -4,34 +4,43 @@ import requests
 from sys import argv
 import re
 from bs4 import BeautifulSoup
+import random
+import unicodedata
+import string
 
-BASE_URL= 'http://www.youtube.com/watch?v='
-EXT = '.mp3'
+def clean_name(title):
+    #some stackoverflow fun, adapted of course
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    cleanedFilename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore')
+    return ''.join(c for c in cleanedFilename if c in valid_chars)
 
-r = requests.get(argv[1])
-print argv[1]
-playlist = re.compile('<ol id=\"watch7-playlist-tray\" class=\"yt-uix-scroller\" .+?</ol>',re.DOTALL).findall(r.text)
+def main():
+    BASE_URL= 'http://www.youtube.com/watch?v='
+    EXT = '.mp3'
+    TEMP_VID = '~/.youtube-dl-%s.flv'
+    LOWER = 100000
+    UPPER = 999999
+    r = requests.get(argv[1])
 
-playlistObj = BeautifulSoup(playlist[0])
+    playlist = re.compile('<ol id=\"watch7-playlist-tray\" class=\"yt-uix-scroller\" .+?</ol>',re.DOTALL).findall(r.text)
 
-videoUrlList = []
+    playlistObj = BeautifulSoup(playlist[0])
 
-for l in playlistObj.find_all('li'):
-    videoUrlList.append((BASE_URL+l.get('data-video-id'),l.get('data-video-title')+EXT))
+    videoUrlList = []
 
-for v in videoUrlList:
-    cmd = 'youtube2mp3 "%s" "%s"' %(v[0],v[1])
-    print cmd
-    os.system(cmd)
+    for l in playlistObj.find_all('li'):
+        videoUrlList.append((BASE_URL+l.get('data-video-id'),l.get('data-video-title')+EXT))
 
+    for v in videoUrlList:
+        temp_file = TEMP_VID % random.randint(LOWER,UPPER)
+        youtube_cmd = 'youtube-dl --output=%s --format=18 "%s"' %(temp_file,v[0])
+        conv_cmd = 'ffmpeg -i %s -acodec libmp3lame -ac 2 -ab 320k -vn -y "%s"' %(temp_file,clean_name(v[1]))
 
+        os.system(youtube_cmd)
+        os.system(conv_cmd)
+        os.remove(temp_file)
 
-
-
-
-
-
-
+main()
 
 
 
